75a2e5e8a870387ab74c36a8c7d27840a40cfba8
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-15 13:54:16 -0500

    setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,12 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    rules: [
+        {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }
+    ]   
 }
