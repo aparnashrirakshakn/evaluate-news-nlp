82b8c4874410333d93ac3d259ac4e6b041033488
Aparna Shriraksha K.N <61300593+aparnashrirakshakn@users.noreply.github.com>
2020-05-16 15:23:27 -0500

    solve css references and add ui (#5)

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack')
 
@@ -10,26 +11,31 @@ module.exports = {
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
-        library: 'Client'
+        library: 'Client',
+        path: path.resolve(__dirname, 'dist')
     },
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
-    rules: [
-        {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        },
-        {
-            test: /\.scss$/,
-            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-        }
-    ],
+    module:{
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebPackPlugin({
             template:"./src/client/views/index.html",
             filename:"./index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
