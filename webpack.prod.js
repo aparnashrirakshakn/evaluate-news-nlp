f379a1e9d5ceaf32151a91b74b13b595e68fab24
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-16 11:49:46 -0500

    install css plugins and configure webpack config for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,29 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     rules: [
         {
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
+        },
+        {
+            test: /\.scss$/,
+            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
         }
     ],
     plugins: [
