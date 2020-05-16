a9ea4042f3faf28fb54d837b7843962e379a3351
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-16 16:11:57 -0500

    add endpoint in server

--- src/server/index.js
+++ src/server/index.js
@@ -11,10 +11,6 @@ var textapi = new AYLIENTextAPI({
   application_key: process.env.API_KEY
 });
 
-app.get('/', function(req, res){
-    res.sendFile('dist/index.html');
-})
-
 app.use(express.static('dist'));
 
 const port = 8081;
@@ -22,4 +18,27 @@ app.listen(port, listener);
 
 function listener() {
     console.log(`Server is listening on port ${port}`);
+}
+
+// app endpoints
+
+app.get('/', function(req, res){
+  res.sendFile('dist/index.html');
+})
+
+app.get('/news', evaluateNews);
+
+function evaluateNews(req, res) {
+    if(req.body()) {
+      textapi.sentiment({
+        url: req.body()
+      }, function(error, response) {
+        if (error === null) {
+          res.send(response);
+        }
+      });
+    }
+    else {
+      res.send("Please check the value of the URL entered. Something seems to be wrong!");
+    }
 }
