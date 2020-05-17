7b7558ea094d2a27f64307bc46500529a3e3a47e
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-17 11:38:23 -0500

    call api from client

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
-
 const dotenv = require('dotenv');
+const AYLIENTextAPI = require('aylien_textapi');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 dotenv.config();
 
 // setup AYLIEN API
-var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id: process.env.APP_ID,
   application_key: process.env.API_KEY
@@ -26,15 +32,19 @@ app.get('/', function(req, res){
   res.sendFile('dist/index.html');
 })
 
-app.get('/news', evaluateNews);
+app.post('/news', evaluateNews);
 
 function evaluateNews(req, res) {
-    if(req.body()) {
+    if(req.body) {
       textapi.sentiment({
-        url: req.body()
+        url: req.body.text
       }, function(error, response) {
         if (error === null) {
+          console.log(response);
           res.send(response);
+        }
+        else {
+          res.send(error);
         }
       });
     }
